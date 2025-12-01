6b99bb0e66a4d1eece0540e1c081f8caeb1298b3
MFC\neverov <rollerkrick@gmail.com>
2025-12-01 12:00:15 +0700

    add guid

---
+++ start.bat
@@ -0,0 +1,14 @@
+@echo off
+chcp 65001 >nul
+
+rem ---- Имя процесса без .exe ----
+set "PROC=main"
+
+tasklist /FI "IMAGENAME eq %PROC%.exe" | find /I "%PROC%.exe" >nul
+if %ERRORLEVEL%==0 (
+    echo Процесс уже запущен. Выходим.
+    exit /b 0
+)
+
+rem ---- Запуск твоего exe ----
+start "" "%~dp0main.exe"
