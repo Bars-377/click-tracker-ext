648f2821a99d494b725291e0883366d2d0d739a1
Неверов Вячеслав Вячеславович <neverov@mfc.tomsk.ru>
2025-12-05 15:13:19 +0700

    browser settings

---
+++ browser.bat
@@ -0,0 +1,5 @@
+@echo off
+start "" "C:\Program Files\Google\Chrome\Application\chrome.exe" --app=https://gosuslugi.ru --start-maximized
+start "" "C:\Program Files\Google\Chrome\Application\chrome.exe" --app=https://vk.com --start-maximized
+start "" "C:\Program Files\Google\Chrome\Application\chrome.exe" --app=https://youtube.com --start-maximized
+exit
